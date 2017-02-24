28b7639f2bc9989558e9cd77ccc85e9ea06039e7
Guillaume Rousseau <roxons333@hotmail.fr>
2017-02-24 19:31:33 +0100

    Created first test

---
+++ neKoApi/test/index.js
@@ -0,0 +1,20 @@
+// launching the server
+process.env.NODE_ENV = 'test';
+var server = require('../server.js');
+
+// packages used to run tests
+var chai = require('chai');
+var expect = chai.expect;
+var request = require('supertest');
+var request = request('http://localhost:8085');
+
+describe('Tests backend API', function(){
+  describe('Testing server responses', function(){
+    it('Should return 200 when GET on /', function(done){
+      request
+        .get('/test')
+        .expect(200)
+        .end(done);
+    });
+  });
+});
