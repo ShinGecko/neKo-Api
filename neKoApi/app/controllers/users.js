82a8e8d178ac6ec3f4b9b92821db6504a07d2e19
Guillaume Rousseau <roxons333@hotmail.fr>
2017-02-26 14:01:22 +0100

    renamed user.js to users.js

---
+++ neKoApi/app/controllers/users.js
@@ -0,0 +1,19 @@
+var _ = require('lodash');
+
+module.exports.auth = function (req, res, next) {
+  User.findOne({ login: req.body.login, password: req.body.password }, function(err, model) {
+    if (err) throw err
+    if(model) {
+      res.send(model._id);
+      Auth.userService.isConnected = true;
+      res.end();
+    }
+    else {
+      res.writeHead(401,{
+        'Content-Type': 'text/plain'
+      });
+      res.write("Wrong Login or Password");
+      res.end();
+    }
+  });
+}
