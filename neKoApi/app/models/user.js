326fa32ba14184e6cd7a48dbb3d7d2e9e1b02a0a
Guillaume Rousseau <roxons333@hotmail.fr>
2017-02-27 00:03:25 +0100

    installed ORM Thinky, created users' model

---
+++ neKoApi/app/models/user.js
@@ -0,0 +1,17 @@
+var thinky = require('thinky')();
+var type = thinky.type;
+var r = thinky.r;
+
+var User = thinky.createModel("User", {
+  id: type.String(),
+  login: {_type: type.String(), enforce_type: "strict"},
+  password: {_type: type.String(), enforce_type: "strict"},
+  email: {_type: type.String(), enforce_type: "strict"},
+  age: type.number().default({
+    if(this.birthDate){
+      return this.birthDate.prototype.getFullYear() - r.now().prototype.getFullYear();
+    }
+    return null;
+  }),
+  birthDate: Date,
+});
