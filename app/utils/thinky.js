57432253938144b4de797ac5e0b8158afc0dc55e
JustinRlle <justin.rerolle@protonmail.com>
2017-03-09 21:43:25 +0100

    thinky: add support for custom config

--- app/utils/thinky.js
+++ app/utils/thinky.js
@@ -1,6 +1,19 @@
 // file: utils/thinky.js
-const thinky = require('thinky')({
-// thinky's options
-})
+const thinky = require('thinky')
 
-module.exports = thinky
+const config = Object.assign({
+  db: 'neko-api'
+}, getCustomConfig())
+
+const thinkyInstance = thinky(config)
+
+module.exports = thinkyInstance
+
+function getCustomConfig() {
+  try {
+    return require('../../config.js') // eslint-disable-line import/no-unresolved
+  } catch (err) {
+    console.log('WARINING: using defaults for database connection')
+    return {}
+  }
+}
