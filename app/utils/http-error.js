f5490def77381a9c2480654fdee9a4cd988794ce
Justin Rerolle <justin.rerolle@sap.com>
2017-03-14 15:19:46 +0100

    http-error: deprecate in favor of ctx.throw

--- app/utils/http-error.js
+++ app/utils/http-error.js
@@ -8,8 +8,10 @@ class HttpError extends Error {
    * Create an HttpError.
    * @param {String} msg explaination of the error
    * @param {Number} status HTTP status code ([list of status codes](https://en.wikipedia.org/wiki/List_of_HTTP_status_codes))
+   * @deprecated
    */
   constructor(msg, status) {
+    console.error('WARINING: HttpError should\'nt be used, please use `ctx.throw` instead') // deprecated
     super(msg)
     this.status = status
     this.name = 'HttpError'
