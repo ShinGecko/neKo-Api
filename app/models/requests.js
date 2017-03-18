9c98cebcc68cf2d86c6602faec8b31a4703a7aca
Guillaume Rousseau <roxons333@hotmail.fr>
2017-03-19 00:27:54 +0100

    models/requests: added JSON-based request

--- app/models/requests.js
+++ app/models/requests.js
@@ -2,5 +2,6 @@ const r = require('./../utils/thinky').r
 
 const reqSingleArg = async (model, field, value) => model.filter(r.row(field).eq(value)).run()
 const reqDoubleArg = async (model, field, value, field2, value2) => model.filter(r.row(field).eq(value).and(r.row(field2).eq(value2))).run()
+const reqFreeArg = async (model, json) => model.filter(json).run()
 
-module.exports = { reqSingleArg, reqDoubleArg }
+module.exports = { reqSingleArg, reqDoubleArg, reqFreeArg }
