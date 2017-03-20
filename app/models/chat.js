947ffb58f2f8b979e16b9054159f52204833ab79
JustinRlle <justin.rerolle@protonmail.com>
2017-03-20 23:51:28 +0100

    chat: finish model

--- app/models/chat.js
+++ app/models/chat.js
@@ -10,18 +10,18 @@ const nameRegex = /^[\w-]+$/
 const types = {
   group: 'group',
   private: 'private',
-  general: 'general', // TODO: general => public?
+  general: 'general',
 }
 
 const validateChatType = () => {
   if (this.type === types.group) {
-    return this.metadata.idGroup
+    return this.idGroup
   }
   if (this.type === type.private) {
-    return this.metadata.idUser1 && this.metadata.idUser2 &&
-      this.metadata.idUser1 !== this.metadata.idUser2
+    return this.idUser1 && this.idUser2 &&
+      this.idUser1 !== this.idUser2
   }
-  if (this.type === types.group) {
+  if (this.type === types.general) {
     return true // check that there is no duplicates
   }
   return false
@@ -31,20 +31,20 @@ const Chat = thinky.createModel('Chat', {
   id: type.string(),
   name: type.string().min(1).regex(nameRegex),
   type: type.string().enum(Object.values(types)),
-  metadata: type.object().schema({
-    idUser1: type.string().optional(),
-    idUser2: type.string().optional(),
-    idGroup: type.string().optional(),
-  })
+  idUser1: type.string().optional(), // for private chat
+  idUser2: type.string().optional(), // for private chat
+  idGroup: type.string().optional(), // for group chat
 })
 
-// TODO: many to many general chat to users
+Chat.hasAndBelongsToMany(User, 'users', 'id', 'id') // for group and public chat
 
-Chat.belongsTo(User, 'user', 'idUser1', 'id')
-Chat.belongsTo(User, 'user', 'idUser2', 'id')
+Chat.belongsTo(User, 'user', 'private.idUser1', 'id')
+Chat.belongsTo(User, 'user', 'private.idUser2', 'id')
 Chat.belongsTo(Group, 'group', 'idGroup', 'id')
 
 Chat.pre('save', validateChatType)
 Chat.pre('validate', validateChatType)
+
+// TODO: add custom getters for differents kind of Chat
 
 module.exports = Chat
