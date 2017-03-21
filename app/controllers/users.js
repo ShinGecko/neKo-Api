3792b208c8bc9cfefb2adc2290d2ebfc4a21e240
Guillaume Rousseau <roxons333@hotmail.fr>
2017-03-21 22:31:44 +0100

    bugfix: bad require name

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -11,7 +11,7 @@ module.exports.auth = async function (ctx) {
   } else {
     type = 'email'
   }
-  const user = await requests.reqDoubleArg(model.users, type, ctx.request.body.login, 'password', ctx.request.body.password)
+  const user = await requests.reqDoubleArg(model, type, ctx.request.body.login, 'password', ctx.request.body.password)
   if (user[0]) {
     ctx.status = 200
   } else {
@@ -23,11 +23,16 @@ module.exports.create = async function (ctx) {
   if (!emailEx.test(ctx.request.body.email) || !logEx.test(ctx.request.body.login) || ctx.request.body.password.length < 7) {
     ctx.throw(401, `Infos sent didn't match requirements`)
   } else {
-    await model.users.save({
+    await model.save({
       login: ctx.request.body.login,
       password: ctx.request.body.password,
       email: ctx.request.body.email,
     })
     ctx.status = 200
   }
+}
+
+/* eslint-disable no-unused-vars */
+module.exports.getGroups = async function (ctx) {
+  // requests.reqFreeArg()
 }
